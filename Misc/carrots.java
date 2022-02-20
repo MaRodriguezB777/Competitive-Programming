fa5751070f60718fd34e8983e979917a3a194445
Manuel Rodriguez <100057293+MaRodriguezB777@users.noreply.github.com>
2022-02-20 17:31:02 -0500

    Trivial

---
+++ Misc/carrots.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class carrots {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        scan.nextInt();
+        System.out.println(scan.nextInt());
+        scan.close();
+    }
+}
